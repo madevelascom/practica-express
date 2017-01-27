e0172698a1c149455431fd81732da0ab7d0161e4
lkuffo <lkuffo@espol.edu.ec>
2017-01-27 04:00:57 -0500

    Crear Tarea (Completado) + Delete a medias

--- routes/tareas.js
+++ routes/tareas.js
@@ -4,11 +4,19 @@ var Proyecto = require('../models/proyectos');
 var Tarea = require('../models/tareas');
 
 router.post('/', function(req, res, next){
-    console.log("hola");
-    console.log(req.body.titulo);
-    console.log(req.query.titulo);
     var tarea = new Tarea({titulo : req.body.titulo , descripcion : req.body.descripcion , responsable : req.body.responsable , estado : 1 , proyecto : "588ac31b793b62329486106d"});
 
+    tarea.save(function(err){
+        if (err) return handleError(err);
+    });
+    res.send("Tarea Guardada Con Exito")
+});
+
+router.delete('/:idTarea', function(req, res, next){
+    var id = req.params.idTarea;
+    Tarea.remove({
+        titulo: id
+    });
     tarea.save(function(err){
         if (err) return handleError(err);
     });
